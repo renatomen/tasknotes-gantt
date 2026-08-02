d585d67a97d6af4b65c56826ab1f397fe75734dc
Renato Mendonca <renatomen@gmail.com>
2026-08-03 01:24:27 +1200

    fix: use intrinsic primitive conversion

--- src/stringifyPrimitive.ts
+++ src/stringifyPrimitive.ts
@@ -4,5 +4,6 @@ export function stringifyDirectPrimitive(value: DirectPrimitive): string {
   if (typeof value === 'string') return value;
   if (typeof value === 'boolean') return value ? 'true' : 'false';
   if (typeof value === 'symbol') return Symbol.prototype.toString.call(value);
-  return `${value}`;
+  if (typeof value === 'number') return Number.prototype.toString.call(value);
+  return BigInt.prototype.toString.call(value);
 }
